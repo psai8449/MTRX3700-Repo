fcbf2efa180d5b9eefb9d8d0317c848bd6787d67
Jestin12 <jestinji@outlook.com>
2023-10-29 01:07:58 +1100

    Robot + Prithik's proximity sensor modules

--- Robot/top_level.sv
+++ Robot/top_level.sv
@@ -160,7 +160,7 @@ logic tx_valid;
 logic tx_ready;
 
 assign tx_valid = 1'b1;
-assign tx_byte = {motor_stat, ,1'b0};
+assign tx_byte = {motor_stat, proximity[7:4], 1'b0};
 
 
 uart_tx #(.CLKS_PER_BIT(50_000_000/9600)) uart_tx_u (
@@ -171,6 +171,58 @@ uart_tx #(.CLKS_PER_BIT(50_000_000/9600)) uart_tx_u (
 	.data_to_send	(tx_byte)
 );
 
+//*************************************************************************
+
+//************************ Proximity **************************************
+
+  logic start, reset, measurement_trigger;
+  logic echo, trigger;
+  logic [7:0] proximity_stat;
+
+  assign echo = GPIO[35];
+  assign GPIO[34] = trigger;
+
+  // Add a timer for measurements
+  always @(posedge CLOCK_50) begin
+    if (measurement_trigger)
+      start <= 1'b1;
+    else
+      start <= 1'b0;
+  end
+
+
+
+  sensor_driver u0(
+    .clk			(CLOCK_50),
+    .rst			(reset),
+    .measure	(start), // Measure on trigger
+    .echo		(echo),
+    .trig		(trigger), 
+    .distance	(proximity_stat)
+  );
+
+  // Add a 0.5-second timer
+  reg [31:0] counter = 0;
+  parameter COUNT_MAX = 25_000_000; // Adjust for your CLOCK_50 frequency
+
+  always @(posedge CLOCK_50) begin
+    if (reset) begin
+      measurement_trigger <= 1'b0; // Reset the measurement trigger
+      counter <= 0;
+    end else if (measurement_trigger) begin
+      measurement_trigger <= 1'b0; // Reset the trigger after measurement
+      counter <= 0;
+    end else begin
+      if (counter == COUNT_MAX) begin
+        counter <= 0;
+        measurement_trigger <= 1'b1; // Trigger measurement every 0.5 seconds
+      end else begin
+        counter <= counter + 1;
+      end
+    end
+  end
+  
+  //**************************************************************************
 
 
 endmodule
